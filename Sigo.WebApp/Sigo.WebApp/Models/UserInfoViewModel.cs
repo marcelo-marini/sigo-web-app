93a38657443d9c1fb2fa7bbb9b7e9128c6fd5b5e
Marcelo Marini <marcelomarini17@hotmail.com>
2021-03-29 20:30:36 -0300

    :tada: Commit Inicial

---
+++ Sigo.WebApp/Sigo.WebApp/Models/UserInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sigo.WebApp.Models
+{
+    public class UserInfoViewModel
+    {
+        public Dictionary<string, string> UserInfoDictionary { get; private set; } = null;
+
+        public UserInfoViewModel(Dictionary<string, string> userInfoDictionary)
+        {
+            UserInfoDictionary = userInfoDictionary;
+        }
+    }
+}
