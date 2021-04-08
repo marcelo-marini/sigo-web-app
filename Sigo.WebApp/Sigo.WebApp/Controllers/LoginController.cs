a34f13da8954a49153e9d1b0dda4fcfb2a637977
Marcelo Marini <marcelomarini17@hotmail.com>
2021-04-08 09:26:27 -0300

    Melhorias no layout

---
+++ Sigo.WebApp/Sigo.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sigo.WebApp.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Home/Views/Index.cshtml");
+        }
+    }
+}
