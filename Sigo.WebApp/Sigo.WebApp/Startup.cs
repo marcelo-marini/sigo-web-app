416103607c5d2f96d82715a9d5d5e4c6f7e18673
Marcelo Marini <marcelomarini17@hotmail.com>
2021-04-08 01:03:10 -0300

    Ajuste

--- Sigo.WebApp/Sigo.WebApp/Startup.cs
+++ Sigo.WebApp/Sigo.WebApp/Startup.cs
@@ -98,6 +98,11 @@ namespace Sigo.WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCookiePolicy(new CookiePolicyOptions { 
+            
+                MinimumSameSitePolicy =  Microsoft.AspNetCore.Http.SameSiteMode.Lax
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
